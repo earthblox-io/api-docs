808766a0c8a7b32da771a75e6cf6a1a861fd1237
Abigail brook-Petty <a.brook-petty@earthblox.io>
2023-10-10 11:16:11 +0100

    main

--- leaflet/map.js
+++ leaflet/map.js
@@ -1,8 +1,10 @@
 // Create a map and set the center and zoom level
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([12, 104], 5);
 
 // Add a base layer (e.g., OpenStreetMap)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
 }).addTo(map);
+
+L.tileLayer('https://earthengine.googleapis.com/v1alpha/projects/ebx-trial-dev/maps/ff32a3c815520054b01b18b884316d0a-89e9b9ebe44ac143bc2fa9ecc77e5a45/tiles/{z}/{x}/{y}').addTo(map);
 
